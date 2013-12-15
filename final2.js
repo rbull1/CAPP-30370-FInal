b90adcbb1dd31a2a5d5ec2fde247a35dbbc623fc
rbull1 <rbull1@nd.edu>
2013-12-15 01:08:37 -0500

    Initial Commit

---
+++ final2.js
@@ -0,0 +1,32 @@
+/**
+ * @author Ryan Bull
+ */
+
+$('#font-button').on("click", function(){
+		var font_size1 = $('#font-size').val();
+		$('#to-style').css("font-size", font_size1);
+		}); 
+		
+console.log('width: ' + $(window).width());
+if($(window).width() > 700) {
+	$('.nav').show();
+};
+
+$(window).resize(function(e) {
+	console.log('width: ' +$(this).width());
+	if($(window).width > 700) {
+		$('.nav').show()
+	}
+});
+
+$(document).on('click',"img", function(e){
+	console.log('alt: ' + $(this).attr('alt'));
+	console.log('width: ' + $(this).width());
+	console.log('height ' + $(this).height());
+});
+
+$(document).on('click', "h1,h2,h3,h4,h5,h6", function(e){
+	console.log('Text: ' + $(this).text());
+	console.log('Color: ' + $(this).css('color'));
+	console.log('Font-Size: ' + $(this).css('font-size'));
+});
